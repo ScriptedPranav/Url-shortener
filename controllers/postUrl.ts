8dc7206a055be548deeb3d2918131549aafdae1e
ScriptedPranav <sinhafighterpranav@gmail.com>
2023-06-09 18:23:18 +0530

    PostURL exists fix

--- controllers/postUrl.ts
+++ controllers/postUrl.ts
@@ -8,18 +8,23 @@ async function postUrl(req: Request, res: Response) {
     if (!url) {
       res.status(404).json("URL not found");
     } else {
-      const shortUrl = nanoid(8);
+      const exists = await URL.findOne({ redirectUrl: url });
+      if (!exists) {
+        const shortUrl = nanoid(8);
 
-      const urlData = new URL({
-        urlID: shortUrl,
-        redirectUrl: url,
-      });
+        const urlData = new URL({
+          urlID: shortUrl,
+          redirectUrl: url,
+        });
 
-      const result = await urlData.save();
-      res.status(200).json(result);
+        const result = await urlData.save();
+        res.status(200).json(result);
+      } else {
+        res.status(203).json(exists)
+      }
     }
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 }
 
